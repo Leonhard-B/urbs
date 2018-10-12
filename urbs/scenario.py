605d5909a2cef5c5ab7766573a53f88bda56282b
Leonhard-B <leonhard.balz@freenet.de>
2018-10-12 10:54:15 +0200

    Sicherung. Nächstes Feature (dict statt Dataframe) auf neuem Branch

--- urbs/scenario.py
+++ urbs/scenario.py
@@ -5,18 +5,10 @@ import pdb
 #Added by Leon in order to update constrains for altenrative scenarios
 def alternative_scenario_stock_prices(prob):
     # change stock commodity prices
-    pdb.set_trace()
-    """
-    stock_commodities_only=[]
-    leo2=list(prob.commodity_dict["price"].keys())
-    for i in leo2: (i[2]=="Stock"): prob.commodity["price"][key]=100
-    
-    co = prob.commodity
-    """
-    
     co=pd.DataFrame()
     co.from_dict(prob.commodity_dict)
     stock_commodities_only = (prob.commodity.index.get_level_values('Type') == 'Stock')
+    
     prob.commodity.loc[stock_commodities_only, 'price'] *= 100
     prob.commodity_dict=prob.commodity.to_dict()
     prob.del_component(prob.def_costs)
